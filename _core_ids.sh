7e6da5dcb2074816c1947a8a2351c8a084b6c19a
Francesco PERONACI (PTV Group) <francesco.peronaci@ptvgroup.com>
2024-07-19 15:43:20 +0200

    wip

--- _core_ids.sh
+++ _core_ids.sh
@@ -8,12 +8,12 @@ _get_route_ids() {
     _get_routes $@ | cut -d, -f1 | awk '!seen[$0]++'
     }
 
-_get_shape_ids() {
-    _get_trips $@ | cut -d, -f8 | awk '!seen[$0]++'
-    }
-
 _get_service_ids() {
     _get_trips $@ | cut -d, -f2 | awk '!seen[$0]++'
+    }
+
+_get_shape_ids() {
+    _get_trips $@ | cut -d, -f8 | awk '!seen[$0]++'
     }
 
 _get_stop_ids() {
