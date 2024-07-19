3e9a64809b950366b0b0f0b2f0e94f4d744dffd5
Francesco PERONACI (PTV Group) <francesco.peronaci@ptvgroup.com>
2024-07-19 10:14:22 +0200

    wip

--- tools_serviceid.sh
+++ tools_serviceid.sh
@@ -2,14 +2,14 @@
 
 source ~/Desktop/GTFS/tools/tools.sh
 
+_get_route_ids() {
+    get_trips $@ | tail -n+2 | cut -d, -f1 | awk '!seen[$0]++'
+    }
+
 _get_service_ids() {
     echo $2
     }
 
 _get_trips() {
     awk -F, -vsearch="${2}" '$2 == search' ${1}/trips.txt
-    }
-
-_get_route_ids() {
-    get_trips $@ | tail -n+2 | cut -d, -f1 | awk '!seen[$0]++'
     }
--- tools_tripid.sh
+++ tools_tripid.sh
@@ -2,10 +2,10 @@
 
 source ~/Desktop/GTFS/tools/tools.sh
 
-_get_trip_ids() {
-    echo $2
-    }
-
 _get_route_ids() {
     get_trips $@ | tail -n+2 | cut -d, -f1 | awk '!seen[$0]++'
+    }
+
+_get_trip_ids() {
+    echo $2
     }
